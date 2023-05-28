3a744169c690779676a6cf2a0d79b33f43c43950
Deespider <diptendu.biswas143@gmail.com>
2023-05-29 03:51:48 +0530

    feat(Models): Added model structure for all apps

--- issues/admin.py
+++ issues/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
 
+from issues.models import Issue, Comment
 # Register your models here.
+@register(Issue)
+class Issue(admin.ModelAdmin):
+    search_fields = ('issue_name', 'assignee', 'project_linked')
+    list_display = ('issue_name', 'assignee', 'priority', 'status')
+
+@register(Comment)
+class Comments(admin.ModelAdmin):
+    search_fields = ('issue', 'author' , 'text')
+    list_display = ('issue', 'text',  'author')
--- issues/models.py
+++ issues/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from users.models import User
+from projects.models import Project
 
 # Create your models here.
+class Issue(models.Model):
+    PRIORITIES = (
+        (1, "Low"),
+        (2, "Medium"),
+        (3, "High")
+    )
+    STATUSES = (
+        (1, "ToDo"),
+        (2, "InProgress"),
+        (3, "Done"),
+        (4, "Blocked")
+    )
+    issue_name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    assignee = models.ForeignKey(to=User, on_delete=models.PROTECT, related_name='assigned_issues')
+    priority = models.SmallIntegerField(choices=PRIORITIES, default=1)
+    project_linked = models.ForeignKey(to=Project,on_delete=models.PROTECT, related_name='linked_project')
+    status =models.SmallIntegerField(choices=STATUSES, default=1)
+
+    def __str__(self):
+        return self.issue_name
+    
+class Comment(models.Model):
+    issue = models.ForeignKey(Issue, on_delete=models.PROTECT, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    # Additional fields and methods as needed
+
+    def __str__(self):
+        return self.text[:50]
