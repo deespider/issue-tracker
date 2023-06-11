002281f94834c94e137a572886b32288b45b1f1f
Deespider <diptendu.biswas143@gmail.com>
2023-06-12 02:53:08 +0530

    feat(Admin): Fixed admin for projects app

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
 
+from projects.models import Project
 # Register your models here.
+@register(Project)
+class Project(admin.ModelAdmin):
+    search_fields = ('project_name', 'project_link')
+    list_display = ('project_name', 'project_link')
